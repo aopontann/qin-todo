52de1033a2ce744aaabef1338ea8355e6a4c0b02
aopontann <aopontan0416@gmail.com>
2022-03-01 22:51:09 +0900

    ✨ mysql connect demo docker上のMySQLコンテナに接続できるかのテストコードを作成

---
+++ cmd/mysql.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql","user1:pass@tcp(mysql:3306)/qin-todo")
+	// db, err := sql.Open("mysql","user1:pass@tcp(127.0.0.1:3306)/qin-todo") //ホストPC上から接続する場合
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println("Openエラー")
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Pingエラー")
+	}
+}
