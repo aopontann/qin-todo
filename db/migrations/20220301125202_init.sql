07264efd0498de9341a178a5e0dd2734ac515feb
aopontann <aopontan0416@gmail.com>
2022-03-02 18:31:09 +0900

    🗃️ create table user

--- db/migrations/20220301125202_init.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-CREATE TABLE testdb(id integer);
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-DROP TABLE testdb;
--- +goose StatementEnd
---
+++ tools/database/migrations/20220301125202_init.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE user(
+  id VARCHAR(26) PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(30) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE user;
+-- +goose StatementEnd
