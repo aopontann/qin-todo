e72bde5cd0508bad6902d1b429b7cc2e1d08c051
aopontann <aopontan0416@gmail.com>
2022-03-03 19:52:47 +0900

    🗃️ usersテーブル, todo_listテーブルの作成

--- tools/database/migrations/20220303080442_init_users.sql
+++ tools/database/migrations/20220303080442_init_users.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE user(
+CREATE TABLE users(
   id VARCHAR(26) PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL UNIQUE,
---
+++ tools/database/migrations/20220303080537_init_todo_list.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE todo_list(
+  id VARCHAR(26) PRIMARY KEY NOT NULL,
+  content VARCHAR(255) NOT NULL,
+  completed bit(1) NOT NULL DEFAULT false,
+  execution_date DATETIME,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+  user_id VARCHAR(26) NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE todo_list;
+-- +goose StatementEnd
