9d612a48fbfd94103fdd05348e6f4d1d3da9b100
aopontann <aopontan0416@gmail.com>
2022-03-16 16:37:31 +0900

    ✨ google auth

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
-  "os"
+	"net/http"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"golang.org/x/oauth2"
@@ -14,7 +16,7 @@ func main() {
   if err != nil {
     log.Fatal("Error loading .env file")
   }
-	r := gin.Default()
+
 	conf := &oauth2.Config{
 		ClientID:     os.Getenv("CLIENT_ID"),
 		ClientSecret: os.Getenv("SECRET_KEY"),
@@ -26,32 +28,32 @@ func main() {
 		Endpoint: google.Endpoint,
 	}
 
+	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 
-	r.GET("/auth", func(c *gin.Context) {
-		url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
-		c.JSON(200, gin.H{
-			"url": url,
+	auth := r.Group("/auth")
+	{
+		// google認証画面にリダイレクト
+		auth.GET("/google", func(c *gin.Context) {
+			url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
+			c.Redirect(http.StatusMovedPermanently, url)
 		})
-	})
-
-	r.GET("/callback", func(c *gin.Context) {
-		code := c.Query("code")
-		tok, err := conf.Exchange(oauth2.NoContext, code)
-		if err != nil {
-			log.Fatal(err)
-		}
-		client := conf.Client(oauth2.NoContext, tok)
-		c.JSON(200, gin.H{
-			"token": tok,
-			"client": client.Get("...")
+		// トークン取得エンドポイント
+		auth.GET("/token", func(c *gin.Context) {
+			code := c.Query("code")
+			// 認可コードからトークンを取得する
+			tok, err := conf.Exchange(oauth2.NoContext, code)
+			if err != nil {
+				log.Fatal(err)
+			}
+			c.JSON(200, tok)
 		})
-	})
+	}
 
-	
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
