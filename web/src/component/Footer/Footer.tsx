ebc99cce7ab8acb72ef2b135582d750192a39980
望月祐汰 <mczk9402@gmail.com>
2022-04-03 16:14:30 +0900

    footerの高さを取得してタスクが多い時でも隠れないように調整

--- web/src/component/Footer/Footer.tsx
+++ web/src/component/Footer/Footer.tsx
@@ -1,10 +1,11 @@
-import React, { ReactNode } from 'react';
+import React, { LegacyRef, ReactNode } from 'react';
 
-export const Footer: React.VFC<{ children: ReactNode }> = ({ children }) => {
+export const Footer: React.VFC<{ children: ReactNode; _ref: React.LegacyRef<HTMLElement> }> = ({ children, _ref }) => {
   return (
     <footer
       className='fixed bottom-0 left-0 min-h-[100px] w-full bg-white py-3 px-6'
       style={{ boxShadow: '0px -1px 1px rgba(0, 0, 0, 0.12)' }}
+      ref={_ref}
     >
       {children}
     </footer>
