6869523e3a5acdfb5fa97e6450d24be36f2b695b
aopontann <aopontan0416@gmail.com>
2022-04-18 14:28:33 +0900

    ファイル分割した部分のコードの削除

--- todo_handler.go
+++ todo_handler.go
@@ -1,55 +1,17 @@
 package main
 
 import (
-	"database/sql"
-
 	"github.com/gin-gonic/gin"
 )
 
-type TodoListInfo struct {
-	Id             string          `json:"id"`
-	Content        string          `json:"content"`
-	Completed      bool            `json:"completed"`
-	Execution_date *sql.NullString `json:"execution_date"`
-}
-
-type TodoRequestBody struct {
-	Content        string `json:"content"`
-	Execution_date string `json:"execution_date"`
-}
-
-type PutTodoRequestBody struct {
-	Content        string `json:"content"`
-	Completed      bool   `json:"completed,omitempty"`
-	Execution_date string `json:"execution_date"`
-}
-
 func GetTodoHandler(c *gin.Context) {
-	var (
-		id             string
-		content        string
-		completed      *sql.NullBool
-		execution_date *sql.NullString
-	)
-	var todoList []TodoListInfo
-
 	// middlewareで認証をして成功すると、ここでユーザーIDを取得できる
 	userId := c.MustGet("userId").(string)
 
-	rows, err := db.Query("SELECT id, content, completed, execution_date FROM todo_list WHERE user_id = ? AND (completed = 0 OR execution_date IS NULL OR execution_date > now())", userId)
+	todoList, err := GetTodoList(userId)
 	if err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
-	}
-
-	defer rows.Close()
-	for rows.Next() {
-		err := rows.Scan(&id, &content, &completed, &execution_date)
-		if err != nil {
-			c.JSON(400, gin.H{"error": err.Error()})
-			return
-		}
-		todoList = append(todoList, TodoListInfo{Id: id, Content: content, Completed: completed.Bool, Execution_date: execution_date})
 	}
 
 	c.JSON(200, gin.H{
@@ -58,7 +20,7 @@ func GetTodoHandler(c *gin.Context) {
 }
 
 func PostTodoHandler(c *gin.Context) {
-	var reqb TodoRequestBody
+	var reqb PostTodoRequestBody
 	err := c.ShouldBindJSON(&reqb)
 	if err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
@@ -66,24 +28,14 @@ func PostTodoHandler(c *gin.Context) {
 	}
 
 	userId := c.MustGet("userId").(string)
-	ulid := GetULID()
+	var ulid string
+	ulid, err = CreateTodo(userId, &reqb)
 
-	if reqb.Execution_date == "" {
-		_, err := db.Exec("INSERT INTO todo_list (id, content, user_id) VALUES (?,?,?)", ulid, reqb.Content, userId)
-		if err != nil {
-			c.JSON(500, gin.H{"error": err.Error()})
-			return
-		}
-		c.JSON(201, gin.H{"id": ulid, "content": reqb.Content, "execution_date": nil, "user_id": userId})
-
-	} else {
-		_, err := db.Exec("INSERT INTO todo_list (id, content, execution_date, user_id) VALUES (?,?,?,?)", ulid, reqb.Content, reqb.Execution_date, userId)
-		if err != nil {
-			c.JSON(500, gin.H{"error": err.Error()})
-			return
-		}
-		c.Status(201)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
 	}
+	c.JSON(201, gin.H{"id": ulid, "content": reqb.Content, "execution_date": nil, "user_id": userId})
 }
 
 func PutTodoHandler(c *gin.Context) {
@@ -97,77 +49,20 @@ func PutTodoHandler(c *gin.Context) {
 	userId := c.MustGet("userId").(string)
 	todoId := c.Param("todo_id")
 
-	// ユーザーが作成したToDoかチェック
-	var id string
-	err = db.QueryRow("SELECT id FROM todo_list WHERE id = ? AND user_id = ?", todoId, userId).Scan(&id)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			c.JSON(403, gin.H{"error": "Todos created by other users cannot be updated, or a non-existent todo ID is specified"})
-		} else {
-			c.JSON(500, gin.H{"error": err.Error()})
-		}
-		return
-	}
-
-	tx, err := db.Begin()
-	if err != nil {
-		c.JSON(500, gin.H{"error": err.Error()})
-		return
-	}
-	defer tx.Rollback()
-
-	if reqb.Content != "" {
-		_, err := tx.Exec("UPDATE todo_list SET content = ? WHERE id = ? AND user_id = ? LIMIT 1", reqb.Content, todoId, userId)
-		if err != nil {
-			c.JSON(500, gin.H{"error": err.Error()})
-			return
-		}
-	}
-
-	if reqb.Execution_date != "" {
-		_, err := tx.Exec("UPDATE todo_list SET execution_date = ? WHERE id = ? AND user_id = ? LIMIT 1", reqb.Execution_date, todoId, userId)
-		if err != nil {
-			c.JSON(500, gin.H{"error": err.Error()})
-			return
-		}
-	}
-
-	reqbComp := 0
-	if reqb.Completed {
-		reqbComp = 1
-	}
-	_, err = tx.Exec("UPDATE todo_list SET completed = ? WHERE id = ? AND user_id = ? AND NOT completed = ? LIMIT 1", reqbComp, todoId, userId, reqbComp)
+	err = UpdateTodo(userId, todoId, &reqb)
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
 
-	err = tx.Commit()
-	if err != nil {
-		c.JSON(500, gin.H{"error": err.Error()})
-		return
-	}
 	c.Status(200)
-
 }
 
 func DeleteTodoHandler(c *gin.Context) {
 	userId := c.MustGet("userId").(string)
 	todoId := c.Param("todo_id")
 
-	// ユーザーが作成したToDoかチェック
-	var id string
-	err := db.QueryRow("SELECT id FROM todo_list WHERE id = ? AND user_id = ?", todoId, userId).Scan(&id)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			c.JSON(403, gin.H{"error": "Unable to delete a Todo created by another user, or a non-existent Todo ID is specified"})
-		} else {
-			c.JSON(500, gin.H{"error": err.Error()})
-		}
-		return
-	}
-
-	_, err = db.Exec("DELETE FROM todo_list WHERE id = ? AND user_id = ? LIMIT 1", todoId, userId)
+	_, err := db.Exec("DELETE FROM todo_list WHERE id = ? AND user_id = ? LIMIT 1", todoId, userId)
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
 		return
--- user_handler.go
+++ user_handler.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"database/sql"
 	"io/ioutil"
 	"log"
 
@@ -9,44 +8,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type PutUserRequestBody struct {
-	Name  string `json:"name"`
-	Image string `json:"image"`
-}
-
 func GetUserHandler(c *gin.Context) {
-	var (
-		id         string
-		name       string
-		email      string
-		avatar_url *sql.NullString
-	)
-
 	// middlewareで認証をして成功すると、ここでユーザーIDを取得できる
 	userId := c.MustGet("userId").(string)
 
-	err := db.QueryRow("SELECT id, name, email, avatar_url FROM users WHERE id = ?", userId).Scan(&id, &name, &email, &avatar_url)
+	userInfo, err := GetUser(userId)
 	if err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
+		c.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
 
-	// この記述よくなさそうだから、他にいい方法があるか調べてみる
-	if avatar_url != nil {
-		c.JSON(200, gin.H{
-			"id":         id,
-			"name":       name,
-			"email":      email,
-			"avatar_url": avatar_url.String,
-		})
-		return
-	}
-	c.JSON(200, gin.H{
-		"id":         id,
-		"name":       name,
-		"email":      email,
-		"avatar_url": nil,
-	})
+	c.JSON(200, userInfo)
 }
 
 func PutUserHandler(c *gin.Context) {
